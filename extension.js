ca4d88ed1811fc16501313ad3b0ac5f0c4d7c9ae
linxx5599 <linxx5599@163.com>
2023-12-24 00:30:14 +0800

    refactor: refactor code

--- extension.js
+++ extension.js
@@ -1,163 +1,51 @@
 const vscode = require('vscode');
 const utils = require('./utils');
+const config = require('./config');
+
+//vscode error message
+function showErrorMessage(message) {
+	vscode.window.showErrorMessage(message);
+}
+//get editor
+async function run(fn) {
+	const editor = vscode.window.activeTextEditor;
+	if (editor) {
+		// 获取选中的文本
+		const selection = editor.selection;
+		const selectedText = editor.document.getText(selection);
+		// 在这里可以进行其他操作，使用选中的文本进行处理
+		try {
+			if (fn) {
+				const result = await fn(selectedText);
+				editor.edit(editBuilder => {
+					editBuilder.replace(selection, result);
+				});
+			}
+		} catch (error) {
+			showErrorMessage(error);
+		}
+	} else {
+		showErrorMessage(config.EMPTY_TEXT);
+	}
+}
+const events = {
+	lowerSnakeCase: utils.convertToLowerSnakeCase,
+	upperSnakeCase: utils.convertToUpperSnakeCase,
+	lowerCamelCase: utils.convertToLowerCamelCase,
+	upperCamelCase: utils.convertToLowerCamelCase,
+	lowerKebabCase: utils.convertToLowerKebabCase,
+	upperKebabCase: utils.convertToUpperKebabCase,
+	lowerCase: t => t.toLowerCase(),
+	upperCase: t => t.toUpperCase()
+}
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let lowerSnakeCase = vscode.commands.registerCommand('lowerSnakeCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToLowerSnakeCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let upperSnakeCase = vscode.commands.registerCommand('upperSnakeCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToUpperSnakeCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let lowerCamelCase = vscode.commands.registerCommand('lowerCamelCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToLowerCamelCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let upperCamelCase = vscode.commands.registerCommand('upperCamelCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToUpperCamelCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let lowerKebabCase = vscode.commands.registerCommand('lowerKebabCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToLowerKebabCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let upperKebabCase = vscode.commands.registerCommand('upperKebabCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			// 在这里可以进行其他操作，使用选中的文本进行处理
-			utils.convertToUpperKebabCase(selectedText)
-				.then(result => {
-					editor.edit(editBuilder => {
-						editBuilder.replace(selection, result);
-					});
-				})
-				.catch(error => {
-					vscode.window.showErrorMessage(error);; // 处理错误信息
-				});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let lowerCase = vscode.commands.registerCommand('lowerCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			// 获取选中的文本
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, selectedText.toLowerCase());
-			});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-
-	let upperCase = vscode.commands.registerCommand('upperCase', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const selection = editor.selection;
-			let selectedText = editor.document.getText(selection);
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, selectedText.toUpperCase());
-			});
-		} else {
-			vscode.window.showErrorMessage('No active text editor.');
-		}
-	});
-	context.subscriptions.push(lowerSnakeCase, upperSnakeCase);
-	context.subscriptions.push(lowerCase, upperCase);
+	for (const event in events) {
+		const disposable = vscode.commands.registerCommand(event, () => run(events[event]));
+		context.subscriptions.push(disposable);
+	}
 }
 function deactivate() { }
 module.exports = {
